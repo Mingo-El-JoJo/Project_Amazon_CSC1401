6e7caf97b9d554d5ce3a33f88b8ad600807a605f
Mingo-El-JoJo <96203742+Mingo-El-JoJo@users.noreply.github.com>
2021-12-15 20:53:25 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
 
-int sum(int* arr,int size)
- {
-     int sum=0;
-     arr[0]=0;
-    for(int i=0; i<size ; i++)
-    {
-        sum += arr[i];
-    }
-    return sum;
- }
-
 
 int main()
 {
-    int arr[]={1,2,3,4,5,6};
-    int s= sum(arr,6);
-    printf("The sum is %d \n",s);
-    printf("The first elmt is %d \n",arr[0]);
+
+    printf("Hello§ World");
     return 0;
 }
 
